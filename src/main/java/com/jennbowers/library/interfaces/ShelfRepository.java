15bf49e92f0bad52d7e4122cafd24a01e67b4503
Jenn Bowers <jenniferbowers03@gmail.com>
2017-08-28 22:28:24 -0400

    Created Shelf Repo

---
+++ src/main/java/com/jennbowers/library/interfaces/ShelfRepository.java
@@ -0,0 +1,11 @@
+package com.jennbowers.library.interfaces;
+
+import com.jennbowers.library.models.Shelf;
+import com.jennbowers.library.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShelfRepository extends CrudRepository<Shelf, Long>{
+    Shelf findAllByUser (User user);
+}
