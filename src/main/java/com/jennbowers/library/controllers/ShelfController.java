40310f9e0e11f1643ed78306fee30c21839fbb9f
Jenn Bowers <jenniferbowers03@gmail.com>
2017-08-29 23:07:04 -0400

    Can now successfully edit a shelf

--- src/main/java/com/jennbowers/library/controllers/ShelfController.java
+++ src/main/java/com/jennbowers/library/controllers/ShelfController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 
@@ -46,10 +47,10 @@ public class ShelfController {
     }
 
 //    POST request for searching through books on shelf... on shelf detail page
-    @RequestMapping(value = "/shelf/{shelfId}", method = RequestMethod.POST)
-    public String shelfPost () {
-        return "shelfDetail";
-    }
+//    @RequestMapping(value = "/shelf/{shelfId}", method = RequestMethod.POST)
+//    public String shelfPost () {
+//        return "shelfDetail";
+//    }
 
 //    POST request for adding a shelf... this form will be at the bottom of the list of all the shelves with just a simple name input in the sidebar and a plus icon to add it
     @RequestMapping(value = "/shelf/add", method = RequestMethod.POST)
@@ -67,13 +68,20 @@ public class ShelfController {
 
 //    GET request for shelf edit page
     @RequestMapping("/shelf/{shelfId}/edit")
-    public String editShelf () {
+    public String editShelf (Model model,
+                             @PathVariable("shelfId") Long shelfId) {
+        Shelf shelf = shelfRepo.findOne(shelfId);
+        model.addAttribute("shelf", shelf);
         return "shelfEdit";
     }
 
 //    POST request for shelf edit page
     @RequestMapping(value = "/shelf/{shelfId}/edit", method = RequestMethod.POST)
-    public String editShelfPost () {
+    public String editShelfPost (@PathVariable("shelfId") Long shelfId,
+                                 @RequestParam("name") String name) {
+        Shelf shelf = shelfRepo.findOne(shelfId);
+        shelf.setName(name);
+        shelfRepo.save(shelf);
         return "redirect:/shelf/{shelfId}";
     }
 
