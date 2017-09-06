eea3280a7d17f6c8a8acabbadf27d0655b414e28
Jenn Bowers <jenniferbowers03@gmail.com>
2017-09-06 18:28:27 -0400

    Friend requests successfully show up on touser's request's page

--- src/main/java/com/jennbowers/library/controllers/RequestController.java
+++ src/main/java/com/jennbowers/library/controllers/RequestController.java
@@ -1,14 +1,32 @@
 package com.jennbowers.library.controllers;
 
+import com.jennbowers.library.interfaces.FriendRequestRepository;
+import com.jennbowers.library.interfaces.UserRepository;
+import com.jennbowers.library.models.FriendRequest;
+import com.jennbowers.library.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.security.Principal;
+
 @Controller
 public class RequestController {
+    @Autowired
+    UserRepository userRepo;
+
+    @Autowired
+    FriendRequestRepository friendRequestRepo;
 
     @RequestMapping(value = "/requests", method = RequestMethod.GET)
-    public String request() {
+    public String request(Principal principal,
+                          Model model) {
+        String username = principal.getName();
+        User user = userRepo.findByUsername(username);
+        Iterable<FriendRequest> friendRequests = friendRequestRepo.findAllByTouserAndPending(user, true);
+        model.addAttribute("friendRequests", friendRequests);
         return "requests";
     }
 }
--- src/main/java/com/jennbowers/library/interfaces/FriendRequestRepository.java
+++ src/main/java/com/jennbowers/library/interfaces/FriendRequestRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.repository.CrudRepository;
 public interface FriendRequestRepository extends CrudRepository<FriendRequest, Long> {
     Iterable<FriendRequest> findAllByTouserAndActive(User user, Boolean booleanParam);
     Iterable<FriendRequest> findAllByFromuserAndActive(User user, Boolean booleanParam);
-    Iterable<FriendRequest> findAllByPending(Boolean booleanParam);
+    Iterable<FriendRequest> findAllByTouserAndPending(User user, Boolean booleanParam);
 }
